6fa4f158736418bd59941db99a49002dd39e9ab8
DarkNoriss <darknoriss@gmail.com>
2023-03-15 12:54:29 +0100

    added modal with forum

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
 import "./style.scss";
 
-const myLibrary = [];
-const myForm = document.getElementById("library-form");
-const bookList = document.getElementById("book-list");
-const btnAdd = document.getElementById("btnAdd");
+// const myLibrary = [];
+const myForm = document.querySelector(".library-form");
+const modal = document.querySelector(".modal");
+const btnAdd = document.querySelector(".btnAdd");
 
+btnAdd.addEventListener("click", () => {
+  modal.showModal();
+});
 myForm.addEventListener("submit", submitForm);
 
 function addBookToLibrary(newBook) {
-  // add book to library and clear last list
   myLibrary.push(newBook);
-  bookList.innerHTML = "";
 
-  myLibrary.forEach((element) => {
-    const list = document.createElement("li");
-    const text = document.createElement("p");
+  // myLibrary.forEach((element) => {
+  //   const list = document.createElement("li");
+  //   const text = document.createElement("p");
 
-    text.innerText = `${element.name}, ${element.author}, ${element.pages}`;
-    list.classList.add("book");
-    list.appendChild(text);
-    bookList.appendChild(list);
-  });
+  //   text.innerText = `${element.name}, ${element.author}, ${element.pages}`;
+  //   list.classList.add("book");
+  //   list.appendChild(text);
+  //   bookList.appendChild(list);
+  // });
 }
 
 function submitForm(event) {
-  // prevent form to send info to server
-  event.preventDefault();
-
-  // create new book then add to library
   const bookName = event.target["book-name"].value;
   const bookAuthor = event.target["book-author"].value;
   const bookPages = event.target["book-pages"].value;
@@ -37,7 +34,4 @@ function submitForm(event) {
     pages: bookPages,
   };
   addBookToLibrary(book);
-
-  // reset the form
-  myForm.reset();
 }
