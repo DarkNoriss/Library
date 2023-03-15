f60208e9f2b18a86fa8d7f234d378c4286cc81d3
DarkNoriss <darknoriss@gmail.com>
2023-03-15 15:30:12 +0100

    added toggle read and remove function :^)

--- main.js
+++ main.js
@@ -23,14 +23,28 @@ function addBookToLibrary(newBook) {
     textAuthor.innerText = `${element.author}`;
 
     const textPages = document.createElement("p");
-    textPages.innerText = `${element.author}`;
+    textPages.innerText = `${element.pages}`;
+
+    const btnRead = document.createElement("button");
+    btnRead.innerText = "Not read";
+    btnRead.classList.add("rounded");
+    btnRead.style.backgroundColor = "#FF7F7F";
+    btnRead.addEventListener("click", toggleRead);
+
+    const btnRemove = document.createElement("button");
+    btnRemove.innerText = "Remove";
+    btnRemove.classList.add("rounded");
+    btnRemove.addEventListener("click", deleteDiv);
 
     const bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
+    bookDiv.classList.add("rounded");
 
     bookDiv.appendChild(textName);
     bookDiv.appendChild(textAuthor);
     bookDiv.appendChild(textPages);
+    bookDiv.appendChild(btnRead);
+    bookDiv.appendChild(btnRemove);
 
     bookGrid.appendChild(bookDiv);
   });
@@ -40,12 +54,38 @@ function submitForm(event) {
   const bookName = event.target["book-name"].value;
   const bookAuthor = event.target["book-author"].value;
   const bookPages = event.target["book-pages"].value;
+
   const book = {
     name: bookName,
     author: bookAuthor,
     pages: bookPages,
   };
+
   addBookToLibrary(book);
 
   myForm.reset();
+}
+
+function toggleRead(event) {
+  const btnColor = event.target.style.backgroundColor;
+
+  if (btnColor == "rgb(255, 127, 127)") {
+    event.target.style.backgroundColor = "#57F287";
+    event.target.innerText = "Read";
+  } else {
+    event.target.style.backgroundColor = "#FF7F7F";
+    event.target.innerText = "Not read";
+  }
+}
+
+function deleteDiv(event) {
+  const bookName = event.target.parentNode.firstChild.innerText;
+
+  myLibrary.forEach((element, index) => {
+    console.log(element);
+    if (element.name == bookName) {
+      myLibrary.splice(index, 1);
+      event.target.parentNode.remove();
+    }
+  });
 }
